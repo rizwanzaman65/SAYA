0e964e574627aba69180ba3ffeb776904dacf818
Rizwan <rizwan2203e@aptechgdn.net>
2022-09-30 11:36:15 +0500

    SAYA

---
+++ script.js
@@ -0,0 +1,66 @@
+// function change1()
+// {
+// document.getElementById("img4").src="img/pic14.webp"
+// }
+
+// function change2()
+// {
+// document.getElementById("img4").src="img/pic4.webp"
+// }
+
+// function change3()
+// {
+// document.getElementById("img5").src="img/pic15.webp"
+// }
+
+// function change4()
+// {
+// document.getElementById("img5").src="img/pic5.webp"
+// }
+
+// function change5()
+// {
+// document.getElementById("img6").src="img/pic16.webp"
+// }
+
+// function change6()
+// {
+// document.getElementById("img6").src="img/pic6.webp"
+// }
+
+// function change7()
+// {
+// document.getElementById("img7").src="img/pic17.webp"
+// }
+
+// function change8()
+// {
+// document.getElementById("img7").src="img/pic7.webp"
+// }
+
+// function change9()
+// {
+// document.getElementById("img8").src="img/pic18.webp"
+// }
+
+// function change10()
+// {
+// document.getElementById("img8").src="img/pic8.webp"
+// }
+
+// function change11()
+// {
+// document.getElementById("img02").src="img/pic19.webp"
+// }
+
+// function change12()
+// {
+// document.getElementById("img02").src="img/pic2.webp"
+// }
+
+
+
+
+
+
+
